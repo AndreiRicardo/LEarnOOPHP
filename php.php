90c14344c246af0b1c702a144fd5e128d18e9a08
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-09 12:53:48 -0300

    exercicio video 5
    variaveis arrays e controles

--- php.php
+++ php.php
@@ -1,3 +1,11 @@
 <?php
-    echo 'ta funfando!';
-?>
+/* usando variaveis
+    $minhaVariavel = 'Deu certo usar a variável';
+    echo $minhaVariavel;
+    */
+    /* crinado simples arrays
+    */ 
+    $numeros = array(10,11,12,13,14,15,16,17,18);
+   
+    print_r($numbers);
+    ?>
