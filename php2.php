8a693a2690f43561b2ef8d44f75fcdca5e486a0a
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-09 15:23:15 -0300

    continua aula 6

--- php2.php
+++ php2.php
@@ -1,6 +1,23 @@
 <?php
-/*este cinteudo refere-se a aula6
-*/
+/*este cinteudo refere-se a aula6 um pouco de funções
 
+    function imprimir(){
+        echo 'Aqui foi usado uma função para imprimir uma menssagem';
+    }
+    imprimir();
+
+//passando parametros para função
+    function EscreveNome($name){
+        print "nome ".$name;
+    }
+    EscreveNome('Andrei');
+    */
+    // passando mais de um parametro
+
+    function EscreveNome($msg,$name){
+        //print $msg." ".$name; // imprimindo os valores
+        print strtoupper($msg." ".$name);// imprindo em letra maiuscula
+    }
+    EscreveNome('Olá','Andrei');
 
 ?>
