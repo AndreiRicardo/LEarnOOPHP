683be474089506001fdfad65fb263b5b0d6bdac5
andreiRicardo <andreiricardorodrigues@gmail.com>
2017-07-09 13:18:46 -0300

    aula 5

--- php.php
+++ php.php
@@ -2,10 +2,22 @@
 /* usando variaveis
     $minhaVariavel = 'Deu certo usar a variável';
     echo $minhaVariavel;
-    */
+ */
     /* crinado simples arrays
-    */ 
-    $numeros = array(10,11,12,13,14,15,16,17,18);
+ */ 
+    //$numeros = array(10,11,12,13,14,15,16,17,18);
+    //$numeros = array{10,11,12,13,14,15,16,17,18};
    
-    print_r($numbers);
-    ?>
+    //print_r($numeros);
+    //echo $numeros[5];
+$idade = array(
+    "andrei" => 35,
+    "ricardo" => 36,
+    "rodrigo" => 30,
+    "rafael" => 32
+);
+    mixed array_pop(array $idades);
+    //echo $idades['andrei']; // vai imprimir o valor da posição
+    //echo count($idades);// vai imprimir a quantidade de itens no vetor
+    print_r($idades);
+?>
